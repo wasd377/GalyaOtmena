6eb6b392d1f0217e3447c1ff456b7fafee5a929d
Антон <anton@workout.su>
2024-02-09 16:13:10 +0300

    Initial Commit

---
+++ Halya/HalyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalyaApp.swift
+//  Halya
+//
+//  Created by Natalia D on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct HalyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
